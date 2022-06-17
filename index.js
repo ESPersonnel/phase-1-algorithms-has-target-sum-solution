efbefcc645552df9b16bae2d1e6b74e6fb7139e9
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-17 23:13:23 +0300

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (var i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for(let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Big O: O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  Psuedocode:
+  1. Iterate through the array
+  2. Create a complement variable
+  3. Iterate through the array again
+  4. If the complement is equal to the current element, return true
+  5. Return false
 */
 
 /*
   Add written explanation of your solution here
+  Explanation:
+  1. We iterate through the array and create a complement variable.
+  2. We iterate through the array again and check if the complement is equal to the current element.
+  3. If it is, return true.
+  4. If it is not, return false.
 */
 
 // You can run `node index.js` to view these console logs
